7447f35fe1af2a78f57aa9dea88cc32d816a65da
Mangeshi <thisismangeshi@gmail.com>
2020-04-05 22:09:56 +0530

    Adds divmod

---
+++ Math/mod_divmod.py
@@ -0,0 +1,20 @@
+'''
+Task
+Read in two integers,  and , and print three lines.
+The first line is the integer division  (While using Python2 remember to import division from __future__).
+The second line is the result of the modulo operator: .
+The third line prints the divmod of  and .
+
+Input Format
+The first line contains the first integer, , and the second line contains the second integer, .
+
+Output Format
+Print the result as described above.
+'''
+
+
+from __future__ import division
+a = int(input())
+b = int(input())
+
+print('{}\n{}\n{}'.format((a//b), (a % b), (divmod(a, b))))
